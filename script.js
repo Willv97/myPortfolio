570646d9796aa871704970e776a1ba8e5b841d23
Willv97 <willv1357@gmail.com>
2024-07-11 17:41:46 -0400

    Added new imgs and a resume pdf. Updating js so menu changes when clicked. Changed styling of profile imgs, etc

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Create a function to open and close menu when clicked
-function toogleMenu() {
-    const menu = document.querySelector('menu-links'); //Target elements
-    const icon = document.querySelector('icon');
-    menu.classList.toggleMenu("open") //Add or remove open class when clicked
-    icon.classList.toggleMenu("open")
+// Function to open and close menu
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links"); //Target elements
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")//Add or remove open class when clicked
+    icon.classList.toggle("open")
 }
